a766d9b76cf0669c574fb6c76bb8751170bdfa46
applegrubber <jscottatwood@gmail.com>
2019-04-27 15:43:35 -0600

    uploading my working friend-finder app

---
+++ app/data/friends.js
@@ -0,0 +1,100 @@
+var friends = [
+    {
+        name: "Ahmed",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Humayun_Ahmed_13Nov2010.jpg/220px-Humayun_Ahmed_13Nov2010.jpg",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Michael",
+        photo: "https://www.thelocal.se/userdata/images/article/6d67730d16af04f3f956389d4cc244af808b8381c23b1e3d218ecd792de14fa8.jpg",
+        scores: [
+            "3",
+            "5",
+            "2",
+            "3",
+            "5",
+            "2",
+            "3",
+            "1",
+            "2",
+            "4"
+        ]
+    },
+    {
+        name: "John",
+        photo: "http://www.bigshocking.com/wp-content/uploads/2014/10/Stare-at-a-Random-Person.jpg",
+        scores: [
+            "1",
+            "3",
+            "4",
+            "2",
+            "5",
+            "3",
+            "1",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name: "Lynda",
+        photo: "https://firachamo.files.wordpress.com/2010/01/day-15-lady.jpg",
+        scores: [
+            "4",
+            "3",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4",
+            "3",
+            "1",
+            "2"
+        ]
+    },
+    {
+        name: "Jared",
+        photo: "https://i2.wp.com/chartcons.com/wp-content/uploads/random.jpg?resize=1021%2C580&ssl=1",
+        scores: [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "1"
+        ]
+    },
+    {
+        name: "Trish",
+        photo: "https://connectcoworking.com/wp-content/uploads/image1.jpg",
+        scores: [
+            "3",
+            "4",
+            "2",
+            "4",
+            "3",
+            "4",
+            "2",
+            "4",
+            "4",
+            "3"
+        ]
+    }
+];
+
+module.exports = friends;
